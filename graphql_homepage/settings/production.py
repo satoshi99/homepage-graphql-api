1109f22b9ee27eeae9984cc65d825bba5d9123bf
satoshi <3104nit@gmail.com>
2021-09-17 22:17:00 +0900

    added host url

--- graphql_homepage/settings/production.py
+++ graphql_homepage/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['homepage-graphql-api.herokuapp.com']
 
 
 from graphql_homepage.aws.conf import *
